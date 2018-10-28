ebcfecceae7b33ded063413f716faab97b651130
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-28 23:04:05 +0000

    toggle color

--- toggle.js
+++ toggle.js
@@ -1,9 +1,11 @@
 console.log("connected!!!");
 var button = document.querySelector("button");
 var body= document.querySelector("body");
+var isColored = false;
 button.addEventListener("click", clicked);
 function clicked(){
-    console.log("clicked!");
-    body.style.background= "#ddcb44";
-    alert("Good job, I am so proud of you!");
+    if (isColored){
+        body.style.background= "rgb(255, 233, 182)";
+    }else {body.style.background= "#ddcb44";}
+    isColored =!isColored
 }
